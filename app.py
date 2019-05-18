afa70bc45e9bbfe169e4032df84e61c30b08089d
Esjael <stewartjlapan@gmail.com>
2019-05-17 18:10:16 -0600

    IT WORKS! Just gotta format my HTML and I am SET

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     mars_data = mongo.db.mars_data.find_one()
-    return render_template("index.html", listings=mars_data)
+    return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scraper():
     mars_data = mongo.db.mars_data
     mars_data_data = mars_scraper.scrape()
-    mars_data.update({}, mars_info_data, upsert=True)
+    mars_data.update_many({}, {'$set': mars_data_data}, upsert=True)
     return redirect("/", code = 302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
